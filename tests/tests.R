5278dff2b3a93f38216273f9e49963d5263662bf
NiklasHohmann <67792281+NiklasHohmann@users.noreply.github.com>
2023-02-23 20:49:08 +0100

    Moved content to test_utility_functions.R

--- tests/tests.R
+++ tests/tests.R
@@ -1,32 +1,4 @@
-#### age_to_time ####
-age=c(65,64)
-age_to_time(age)
 
-#### depth_to_height ####
-
-depth = c(200,100)
-depth_to_height(depth)
-
-#### order_with_time_increasing_base ####
-# standard case
-time=c(0,1,2,3)
-height = runif(length(time))
-order_with_time_increasing_base(time,height)
-
-# reversed order
-time = rev(time)
-height = runif(length(time))
-order_with_time_increasing_base(time,height)
-
-# unordered time
-time = c(2,1,3,4)
-height = runif(length(time))
-order_with_time_increasing_base(time,height)
-
-# duplicates in time throws error
-time = c(1,1,2,3)
-height = runif(length(time))
-order_with_time_increasing_base(time,height)
 
 #### time_height_tie_points_to_adm ####
 time=runif(4)
