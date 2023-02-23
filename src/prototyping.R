8a1161e95244980d64268300dfb056d7ba16744c
NiklasHohmann <67792281+NiklasHohmann@users.noreply.github.com>
2023-02-23 16:18:25 +0100

    Add tests & start piecewise constant case

--- src/prototyping.R
+++ src/prototyping.R
@@ -8,69 +8,103 @@ depth_to_height = function(depth){
   return(height)
 }
 
-order_with_time_increasing = function(time, height) {
+condensation_in_length_dim_to_sedrate = function(condensation){
+  if (any(condensation <= 0)){
+    stop("Condensation in spatial domain must be > 0")
+  }
+  sed_rate = condensation^(-1)
+  return(sed_rate)
+}
+
+order_with_time_increasing_base = function(time, vals) {
   if (any(duplicated(time))) {
     stop("Error: Duplicated tie points in time dimension")
   }
   if (!is.unsorted(time,strictly = FALSE)) {
     return(list(time = time,
-                height = height))
+                vals = vals))
   }
   if (is.unsorted(time, strictly = TRUE)) {
     sorted_list=sort(time, 
                      index.return = TRUE)
     time = sorted_list$x
-    height = height[sorted_list$ix]
+    vals = vals[sorted_list$ix]
     return(list(time = time, 
-                height = height))
+                vals = vals))
   }
-  time = rev(time)
-  height = rev(height)
-  if (is.unsorted(time, strictly = TRUE)) {
-    sorted_list=sort(time, 
-                     index.return = TRUE)
-    time = sorted_list$x
-    height = height[sorted_list$ix]
-    return(list(time = time, 
-                height = height))
-  }
-  stop("Disordered tie points in time timension")
 }
 
-time_height_tie_points_to_adm = function(time, height, lmode = "height", tmode = "time") {
-  temp_list = order_with_time_increasing(time = time,
-                                         height = height)
+time_height_tie_points_to_adm = function(time, 
+                                         height, 
+                                         lmode = "height", 
+                                         tmode = "time", 
+                                         interpol_data = NULL, 
+                                         derivative_data = NULL) {
+  
+  temp_list = order_with_time_increasing_base(time = time,
+                                         vals = height)
   adm=list(tpoints = temp_list$time, 
            lpoints = temp_list$height, 
            interpol_mode = "piecewise_linear",
-           interpol_data = NULL, 
-           derivative_data = NULL, 
+           interpol_data = interpol_data, 
+           derivative_data = derivative_data, 
            lmode = lmode, 
            tmode = tmode
            )
   return(adm)
 }
 
-age_height_tie_points_to_adm = function(age, depth, lmode = "height", tmode = "age") {
+time_depth_tie_points_to_adm = function(time, 
+                                        depth, 
+                                        lmode = "depth", 
+                                        tmode = "time") {
+  height = depth_to_height(depth = depth)
+  adm = time_height_tie_points_to_adm(time = time, 
+                                      height = height, 
+                                      lmode = lmode,
+                                      tmode = tmode)
+  return(adm)
+}
+
+age_height_tie_points_to_adm = function(age, 
+                                        depth, 
+                                        lmode = "height", 
+                                        tmode = "age") {
   time = age_to_time(age = age)
   adm = time_height_tie_points_to_adm(time = time, 
                                       height = height, 
                                       lmode = lmode,
                                       tmode = tmode)
+  return(adm)
 }
 
-age_depth_tie_points_to_adm = function(age, depth, lmode = "depth", tmode = "age") {
+age_depth_tie_points_to_adm = function(age, 
+                                       depth, 
+                                       lmode = "depth", 
+                                       tmode = "age") {
   height = depth_to_height(depth = depth)
-  adm = age_height_tie_points_to_adm(time = time, 
-                                      height = height, 
-                                      lmode = lmode,
-                                      tmode = tmode)
-}
-
-time_depth_tie_points_to_adm = function(age, depth, lmode = "depth", tmode = "time") {
-  height = depth_to_height(depth = depth)
+  time = age_to_time(age = age)
   adm = time_height_tie_points_to_adm(time = time, 
                                      height = height, 
                                      lmode = lmode,
                                      tmode = tmode)
+  return(adm)
+}
+
+order_with_time_increasing_mod = function(time, vals) {
+  if (any(duplicated(time))) {
+    stop("Error: Duplicated tie points in time dimension")
+  }
+  # for case where length(time) neq length(vals)
+  if (!is.unsorted(time, strictly = TRUE)) {
+    return(list(time = time,
+                vals = vals))
+  }
+  time = rev(time)
+  if (!is.unsorted(time, strictly = TRUE)) {
+    return(list(time = time,
+                vals = vals))
+  }
+  stop("Could not sort along time dimension")
+  
 }
--- tests/tests.R
+++ tests/tests.R
@@ -1,1 +1,51 @@
-# tests to come soon
+#### age_to_time ####
+age=c(65,64)
+age_to_time(age)
+
+#### depth_to_height ####
+
+depth = c(200,100)
+depth_to_height(depth)
+
+#### order_with_time_increasing_base ####
+# standard case
+time=c(0,1,2,3)
+height = runif(length(time))
+order_with_time_increasing_base(time,height)
+
+# reversed order
+time = rev(time)
+height = runif(length(time))
+order_with_time_increasing_base(time,height)
+
+# unordered time
+time = c(2,1,3,4)
+height = runif(length(time))
+order_with_time_increasing_base(time,height)
+
+# duplicates in time throws error
+time = c(1,1,2,3)
+height = runif(length(time))
+order_with_time_increasing_base(time,height)
+
+#### time_height_tie_points_to_adm ####
+time=runif(4)
+height = runif(length(time))
+time_height_tie_points_to_adm(time,height)
+
+#### time_depth_tie_points_to_adm ####
+time=1:4
+depth = runif(length(time))
+time_depth_tie_points_to_adm(time,height)
+
+#### age_height_tie_points_to_adm ####
+age = seq(66,69,length.out = 4)
+height = runif(length(age))
+
+age_height_tie_points_to_adm(age,height)
+
+#### age_depth_tie_points_to_adm ####
+age = seq(66,69,length.out = 4)
+depth = runif(length(age))
+
+age_depth_tie_points_to_adm(age,depth)
