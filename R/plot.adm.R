646a571b94b41b9bf59c99f7d1004ede033be17a
Niklas Hohmann <67792281+NiklasHohmann@users.noreply.github.com>
2023-10-05 17:49:19 +0200

    Improved plotting

--- R/plot.adm.R
+++ R/plot.adm.R
@@ -1,4 +1,5 @@
-plot.adm = function(adm,...){
+plot.adm = function(adm, lwd_hiat = 1, lwd_cons = 1, lty_hiat = 3, lty_cons = 1, 
+                    col_hiat = "black", col_cons = "black", ...){
   
   #'
   #'
@@ -8,6 +9,22 @@ plot.adm = function(adm,...){
   plot(x = adm$t,
        y = adm$h,
        type = "l",
+       col = "white",
        ...)
+  
+  # non-erosive parts
+  x = replace(adm$t, is_destructive(adm, adm$t, mode = "open"), NA)
+  y = replace(adm$h, is_destructive(adm, adm$t, mode = "open"), NA)
+  graphics::lines(x = x, y = y,
+                  lwd = lwd_cons,
+                  lty = lty_cons,
+                  col = col_cons)
+  
+  # erosive parts
+  x= replace(adm$t, ! is_destructive(adm, adm$t, mode = "closed"), NA)
+  y= replace(adm$h, ! is_destructive(adm, adm$t, mode = "closed"), NA)
+  graphics::lines(x, y, lwd = lwd_hiat, lty = lty_hiat, col = col_hiat)
+  
+}
   
 
