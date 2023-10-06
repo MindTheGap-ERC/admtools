6a70d87aa26d08e49a15bd9eafefbaf11df36a10
Niklas Hohmann <67792281+NiklasHohmann@users.noreply.github.com>
2023-10-06 08:05:46 +0200

    added example data

---
+++ R/CarboCatLite_data.R
@@ -0,0 +1,15 @@
+#' Data exported for CatrboCATLite model run
+#' 
+#' @format A list with the following fields:
+#'  * _time_myr_ : time points in Myr from the model run
+#'  * _height_2_km_offshore_m_ : sediment thickness accumulated 2 km from shore  
+#'  * _height_4_km_offshore_m_ :  sediment thickness accumulated 4 km from shore   
+#'  * _height_6_km_offshore_m_ :  sediment thickness accumulated 6 km from shore 
+#'  * _height_8_km_offshore_m_ :  sediment thickness accumulated 8 km from shore 
+#'  * _height_10_km_offshore_m_ :  sediment thickness accumulated 10 km from shore 
+#'  * _height_12_km_offshore_m_ :  sediment thickness accumulated 2 km from shore 
+#' 
+#' @source Add refs here
+
+
+"CarboCATLite_data"
