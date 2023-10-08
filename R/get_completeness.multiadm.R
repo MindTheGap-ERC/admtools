39212fbfdbc683f85be101a6adbd7d6b4b816880
Niklas Hohmann <67792281+NiklasHohmann@users.noreply.github.com>
2023-10-08 13:36:00 +0200

    - multiadm

--- R/get_completeness.multiadm.R
+++
@@ -1,6 +0,0 @@
-get_completeness.multiadm = function(multiadm){
-  
-  adm_list = split_multiadm(multiadm)
-  
-  completenesses = sapply(seq_along(adm_list), function(x) get_completeness(adm_list[[x]]))
-}
