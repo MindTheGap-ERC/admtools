d091c5ac8397cc46c6290f4017f751e061fa9380
Niklas Hohmann <67792281+NiklasHohmann@users.noreply.github.com>
2023-10-09 09:38:23 +0200

    Update CarboCatLite_data.R

--- R/CarboCatLite_data.R
+++ R/CarboCatLite_data.R
@@ -1,4 +1,6 @@
-#' Data exported for CatrboCATLite model run
+#' 
+#' @description
+#' Data exported from CarboCATLite model run
 #' 
 #' @format A list with the following fields:
 #'  * _time_myr_ : time points in Myr from the model run
@@ -7,9 +9,15 @@
 #'  * _height_6_km_offshore_m_ :  sediment thickness accumulated 6 km from shore 
 #'  * _height_8_km_offshore_m_ :  sediment thickness accumulated 8 km from shore 
 #'  * _height_10_km_offshore_m_ :  sediment thickness accumulated 10 km from shore 
-#'  * _height_12_km_offshore_m_ :  sediment thickness accumulated 2 km from shore 
+#'  * _height_12_km_offshore_m_ :  sediment thickness accumulated 12 km from shore 
+#'  * _eustatic_SL_m_ : eustatic sea level used for the model run.
 #' 
-#' @source Add refs here
+#' @source Elapsed model time, sea level, and accumulated sediment thickness taken from
+#' the [scenario A model run here](https://github.com/MindTheGap-ERC/CarboCATLite/tree/scenarioA)
+#' 
+#' @references
+  #' * Burgess, Peter. "CarboCAT: A cellular automata model of heterogeneous carbonate strata." Computers & geosciences 53 (2013): 129-140. [DOI: 10.1016/j.cageo.2011.08.026](https://doi.org/10.1016/j.cageo.2011.08.026)
+  #' * Burgess, Peter. (2023). CarboCATLite (v1.0.1). Zenodo. [DOI: 10.5281/zenodo.8402578](https://doi.org/10.5281/zenodo.8402578)
 
 
 "CarboCATLite_data"
