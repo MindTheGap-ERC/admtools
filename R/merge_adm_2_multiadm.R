0aa951192737403672854e32ac118fe224a3bde8
Niklas Hohmann <67792281+NiklasHohmann@users.noreply.github.com>
2023-11-27 09:00:46 +0100

    + new

--- R/merge_adm_2_multiadm.R
+++ R/merge_adm_2_multiadm.R
@@ -4,20 +4,27 @@ merge_adm_2_multiadm = function(...){
   #' @title combine multiple adm ojects into multiadm object
   #' 
   #' @param ... adm objects
+  #' 
+  #' @returns a multiadm object
+  #' 
+  #' @export
   
   adm_list = list(...)
   
   multiadm = list("t" = list(),
                   "h" = list(),
-                  "destructive" = list(),
-                  "no_of_entries" = length(adm_list))
+                  "destr" = list(),
+                  "no_of_entries" = length(adm_list),
+                  "T_unit" = NULL,
+                  "L_unit" = NULL)
   for (i in seq_along(adm_list)){
     adm = adm_list[[i]]
     multiadm[["t"]][[i]] = adm$t
     multiadm[["h"]][[i]] = adm$h
-    multiadm[["destructive"]][[i]] = adm$destr
+    multiadm[["destr"]][[i]] = adm$destr
+
   }
-  
+
   class(multiadm) = "multiadm"
   return(multiadm)
 }
