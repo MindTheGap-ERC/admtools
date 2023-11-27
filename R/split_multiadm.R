c9a9712694301c014efce9b0ad7db60ae68beafa
Niklas Hohmann <67792281+NiklasHohmann@users.noreply.github.com>
2023-11-27 09:00:59 +0100

    + new

--- R/split_multiadm.R
+++ R/split_multiadm.R
@@ -1,4 +1,14 @@
 split_multiadm = function(multiadm){
+  
+  #'
+  #' @export
+  #' 
+  #' @title split multiadm objects into adm
+  #' 
+  #' @param multiadm a multiadm object
+  #' 
+  #' @returns a list of adm objects
+  #'
   adm_list = list()
   for (i in seq_len(multiadm$no_of_entries)){
     adm_list[[i]] = tp_2_adm(t = multiadm[["t"]][[i]],
