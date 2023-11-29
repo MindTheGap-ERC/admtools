d6a83e5d8b93e317459239ec28de2221286dca90
Niklas Hohmann <67792281+NiklasHohmann@users.noreply.github.com>
2023-11-29 15:10:36 +0100

    S3 methods for printing

---
+++ R/print.adm.R
@@ -0,0 +1,9 @@
+print.adm = function(x, ...){
+  
+  #'
+  #' @export
+  #' 
+
+  cat("Age-depth model")
+  return(invisible())
+}
---
+++ R/print.multiadm.R
@@ -0,0 +1,9 @@
+print.multiadm = function(x, ...){
+  
+  #'
+  #' @export
+  #' 
+  
+  cat("Portfolio of age-depth models")
+  return(invisible())
+}
