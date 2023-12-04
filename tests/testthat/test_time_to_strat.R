5db65dc2a4f678eb924adb51748f19b0746236c6
Niklas Hohmann <67792281+NiklasHohmann@users.noreply.github.com>
2023-12-04 13:02:27 +0100

    + tests

---
+++ tests/testthat/test_time_to_strat.R
@@ -0,0 +1,16 @@
+test_that(" for phylo objects, tree topology is unaffected by transformation",
+          {
+            adm = tp_to_adm(t = c(0,1,2), h = c(0,2,3))
+            set.seed(1)
+            tree = ape::rlineage(birth = 1,
+                                 death = 0.1,
+                                 Tmax = 2)
+            
+            tree_trans = time_to_strat(obj = tree,
+                                       x = adm)
+            
+            expect_equal(tree$edge, tree_trans$edge)
+            expect_equal(tree$tip.label, tree_trans$tip.label)
+            expect_equal(tree$Nnode, tree_trans$Nnode)
+            
+          })
