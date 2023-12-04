4ed3a46b81fa0981346e86bcd7d2ced44ffba5d7
Niklas Hohmann <67792281+NiklasHohmann@users.noreply.github.com>
2023-12-04 09:31:16 +0100

    + tests

--- tests/testthat/test_is_multiadm.R
+++ tests/testthat/test_is_multiadm.R
@@ -1,4 +1,30 @@
 test_that("Requires multiadm class",
           {
-            expect_false(suppressWarnings(is_multiadm(2)))
+            expect_false(is_multiadm(2))
+            expect_false(is_multiadm(2, quietly = TRUE))
+            expect_warning(is_multiadm(2, quietly = FALSE))
           })
+
+test_that("Correct name fields required",{
+  x = 2
+  class(x) = "multiadm"
+  
+  expect_false(is_multiadm(x))
+  expect_false(is_multiadm(x, quietly = TRUE))
+  expect_warning(is_multiadm(x, quietly = FALSE))
+  
+})
+
+test_that("Requires correct number of names",{
+  x = list("t" = 2,
+           "h" = 2,
+           "destr" = 2,
+           "no_of_entries" = 2,
+           "T_unit" = NULL,
+           "L_unit" = NULL)
+  x[["foo"]] = 2
+  expect_false(is_multiadm(x))
+  expect_false(is_multiadm(x, quietly = TRUE))
+  expect_warning(is_multiadm(x, quietly = FALSE))
+  
+})
