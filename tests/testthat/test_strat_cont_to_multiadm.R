4cb40f76a72d27215108ab37e778db0f7e8f5577
Niklas Hohmann <67792281+NiklasHohmann@users.noreply.github.com>
2024-01-09 14:58:38 +0100

    only one rep

--- tests/testthat/test_strat_cont_to_multiadm.R
+++ tests/testthat/test_strat_cont_to_multiadm.R
@@ -9,7 +9,7 @@ test_that("constant proxy vals return linear adm", {
   strat_cont_gen = function() return(approxfun(x = c(h_min, h_max), y = c(1,1), rule = 2))
   time_cont_gen = function() return(approxfun(x = c(t_min, t_max), y = c(1,1), rule = 2))
   h = seq(h_min, h_max, length.out = no_eval)
-  madm = strat_cont_to_multiadm(h_tp, t_tp, strat_cont_gen, time_cont_gen, h)
+  madm = strat_cont_to_multiadm(h_tp, t_tp, strat_cont_gen, time_cont_gen, h, no_of_rep = 1)
   expect_equal(unname(madm$h[[1]]), h)
   expect_equal(unname(madm$t[[1]]), seq(t_min, t_max, length.out = no_eval))
   
