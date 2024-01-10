d1c3e5aecadf625243856f4e7bca35c9e9d15c8e
Hohmann <n.h.hohmann@uu.nl>
2024-01-10 13:35:44 +0100

    + make legend

--- R/plot.multiadm.R
+++ R/plot.multiadm.R
@@ -77,6 +77,9 @@ move_multiadm_to_env = function(x, ...){
                       "L_unit" = x$L_unit,
                       "h_range" = c(h_min, h_max),
                       "t_range" = c(t_min, t_max),
+                      "median_col" = "red",
+                      "envelope_col" = "blue",
+                      "p_envelope" = 0.95,
                       "madm" = x),
          envir = .adm_plot_env)
   return(invisible())
@@ -103,9 +106,25 @@ plot_envelope = function(){
   for ( i in seq_len(100)){
     h_t[[i]] = sapply(h_list, function(x) x[i])
   }
-  graphics::lines(sapply(h_t, function(x) stats::quantile(x, 0.025, na.rm = TRUE)),h, col = "blue")
-  graphics::lines(sapply(h_t, function(x) stats::quantile(x, 0.975, na.rm = TRUE)),h, col = "blue")
-  graphics::lines(sapply(h_t, function(x) stats::quantile(x, 0.5, na.rm = TRUE)),h, col = "red")
+  graphics::lines(sapply(h_t, function(x) stats::quantile(x, 0.5 * (1 - list$p_envelope) , na.rm = TRUE)),h, col = list$envelope_col)
+  graphics::lines(sapply(h_t, function(x) stats::quantile(x, 0.5 * (1 - list$p_envelope) + list$p_envelope, na.rm = TRUE)),h, col = list$envelope_col)
+  graphics::lines(sapply(h_t, function(x) stats::quantile(x, 0.5, na.rm = TRUE)),h, col = list$median_col)
   
   return(invisible())
+}
+
+make_legend = function(){
+  #' @export
+  #' 
+  #' @title plot legend
+  #' 
+  #' @returns invisible NULL
+  #' 
+  
+  list = get("adm_plot_info", envir = .adm_plot_env)
+  legend("topleft",
+         lwd = 1,
+         lty = 1,
+         col = c(list$envelope_col, list$median_col),
+         legend = c("Median", paste0(list$p_envelope, " Percentile Envelope" )))
 }
