755eb9d155eea9677303795c43484ed5f3855eb9
Niklas Hohmann <67792281+NiklasHohmann@users.noreply.github.com>
2024-02-19 10:58:48 +0100

    add tp helper: normal distribution

--- R/tp_helpers.R
+++ R/tp_helpers.R
@@ -13,6 +13,43 @@ tp_time_floating_scale = function(){
   return(f)
 }
 
+tp_time_norm = function(mean, sd, force_order = TRUE){
+  #' @export
+  #' 
+  #' @title time tie points with normal distribution
+  #' 
+  #' @description
+    #' defines a function factory that returns normally distributed times. FOr usage with `sedrate_to_multiadm` and `strat_cont_to_multiadm`.
+    #' 
+  #' @param mean numeric vector, mean age of tie points
+  #' @param sd numeric vector, standard deviation of tie points
+  #' @param force_order logical, enforce strictly increasing times
+  #' 
+  #' @returns a function
+  if (!force_order){
+    f = function(){
+      return(rnorm(n = length(mean), mean = mean, sd = sd))
+    }
+    return(f)
+  }
+  
+  f = function(){
+    i = 1
+    repeat{
+      t = rnorm(n = length(mean), mean = mean, sd = sd)
+      if (!is.unsorted(t)){
+        break
+      }
+      i = i+1
+      if (i >1000){
+        stop("COuld not find strictly increasing sample")
+      }
+    }
+    return(t)
+  }
+  return(f)
+}
+
 tp_height_det = function(heights){
   #' @export
   #' 
