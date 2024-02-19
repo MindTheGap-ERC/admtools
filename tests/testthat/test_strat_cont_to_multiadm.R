130d2a38ef495978ab53fe291dfcb7b9585587f2
Niklas Hohmann <67792281+NiklasHohmann@users.noreply.github.com>
2024-02-19 15:03:00 +0100

    add tests

--- tests/testthat/test_strat_cont_to_multiadm.R
+++ tests/testthat/test_strat_cont_to_multiadm.R
@@ -1,18 +1,68 @@
-test_that("constant proxy vals return linear adm", {
-  h_min = 0
-  h_max = 1
-  t_min = 0
-  t_max = 1
+test_that("constant proxy vals return linear adm between tps", {
+  h_min = 2
+  h_max = 3
+  t_min = -3
+  t_max = -2
   no_eval = 100
   t_tp = function() return(c( t_min,  t_max))
   h_tp = function() return(c( h_min,  h_max))
   strat_cont_gen = function() return(approxfun(x = c(h_min, h_max), y = c(1,1), rule = 2))
   time_cont_gen = function() return(approxfun(x = c(t_min, t_max), y = c(1,1), rule = 2))
-  h = seq(h_min, h_max, length.out = no_eval)
+  h = seq(h_min + 0.1, h_max - 0.1, length.out = no_eval)
   madm = strat_cont_to_multiadm(h_tp, t_tp, strat_cont_gen, time_cont_gen, h, no_of_rep = 1)
-  expect_equal(unname(madm$h[[1]]), h)
-  expect_equal(unname(madm$t[[1]]), seq(t_min, t_max, length.out = no_eval))
-  
+  expect_equal(madm$h[[1]], h)
+  expect_equal(madm$t[[1]], seq(t_min + 0.1, t_max - 0.1, length.out = no_eval))
+})
+
+test_that("constant proxy vals return linear adm", {
+  h_min = 2
+  h_max = 3
+  t_min = -3
+  t_max = -2
+  no_eval = 100
+  t_tp = function() return(c( t_min,  t_max))
+  h_tp = function() return(c( h_min,  h_max))
+  strat_cont_gen = function() return(approxfun(x = c(h_min, h_max), y = c(1,1), rule = 2))
+  time_cont_gen = function() return(approxfun(x = c(t_min, t_max), y = c(1,1), rule = 2))
+  h = seq(h_min, h_max , length.out = no_eval)
+  madm = strat_cont_to_multiadm(h_tp, t_tp, strat_cont_gen, time_cont_gen, h, no_of_rep = 1)
+  expect_equal(madm$h[[1]], h)
+  expect_equal(madm$t[[1]], seq(t_min, t_max, length.out = no_eval))
+})
+
+test_that("estimation outside of tps works", {
+  h_min = 2
+  h_max = 3
+  t_min = -3
+  t_max = -2
+  no_eval = 100
+  t_tp = function() return(c( t_min,  t_max))
+  h_tp = function() return(c( h_min,  h_max))
+  strat_cont_gen = function() return(approxfun(x = c(h_min, h_max), y = c(1,1), rule = 2))
+  time_cont_gen = function() return(approxfun(x = c(t_min, t_max), y = c(1,1), rule = 2))
+  h = seq(h_min - 1, h_max + 1 , length.out = no_eval)
+  madm = strat_cont_to_multiadm(h_tp, t_tp, strat_cont_gen, time_cont_gen, h, no_of_rep = 1)
+  expect_equal(madm$h[[1]], h)
+  expect_equal(madm$t[[1]], seq(t_min-1, t_max + 1, length.out = no_eval))
+})
+
+
+
+test_that("multiple tie points work",{
+  t_1 = 0
+  t_2 = 1
+  t_3 = 2
+  h_1 = 0
+  h_2 = 1
+  h_3 = 3
+  t_tp = function() return(c(t_1,t_2, t_3))
+  h_tp = function() return(c(h_1,h_2,h_3))
+  strat_cont_gen = function() return(approxfun(x = c(h_1, h_3), y = c(1,1), rule = 2))
+  time_cont_gen = function() return(approxfun(x = c(t_1, t_3), y = c(1,1), rule = 2))
+  h = seq(h_1,h_3, by = 0.1)
+  madm = strat_cont_to_multiadm(h_tp, t_tp, strat_cont_gen, time_cont_gen, h, no_of_rep = 1)
+  expect_equal(madm$h[[1]], h)
+  expect_equal(madm$t[[1]], unique(c(seq(t_1, t_2, by = 0.1), seq(t_2, t_3, by = 0.05))))
 })
 
 test_that("errors in tie point encoding are caught", {
@@ -25,7 +75,7 @@ test_that("errors in tie point encoding are caught", {
   # disorder in time tp
   t_tp = function() return(c( t_max,  t_min))
   h_tp = function() return(c( h_min,  h_max))
-  strat_cont_gen = function() return(approxfun(x = c(t_min, t_max), y = c(1,1), rule = 2))
+  strat_cont_gen = function() return(approxfun(x = c(h_min, h_max), y = c(1,1), rule = 2))
   time_cont_gen = function() return(approxfun(x = c(t_min, t_max), y = c(1,1), rule = 2))
   h = seq(h_min, h_max, length.out = no_eval)
   expect_error(strat_cont_to_multiadm(h_tp, t_tp, strat_cont__gen, time_cont_gen, h, no_of_rep = 1))
