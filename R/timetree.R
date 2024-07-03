9cc4b79d31f6d9b684f9df4cbad7f7c347db406d
Niklas Hohmann <67792281+NiklasHohmann@users.noreply.github.com>
2024-07-03 10:47:10 +0200

    remove dependency from ape pkg

---
+++ R/timetree.R
@@ -0,0 +1,13 @@
+#' @title example time tree
+#' 
+#' @description
+#' Time tree generated using the `ape` package. Code used to generate is
+#' 
+#' set.seed(1)
+#' tree_in_time = ape::rlineage(birth = 1.8,
+#'                              death = 0.2,
+#'                              Tmax = 2)
+#'                              
+#'                              
+#' 
+"timetree"
